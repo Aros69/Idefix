d6d776991460626538d16dffa7a1cdfcfb771499
Cyril <cyril_reymond@hotmail.fr>
2019-03-25 11:00:32 +0100

    Display graph with matplotlib

--- testingcode/exploration/exploration.py
+++ testingcode/exploration/exploration.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import networkx as nx
 from networkx.drawing import nx_agraph
-import pygraphviz as pgv
 import re
 import matplotlib.pyplot as plt
 # first exploration algorithm (just one robot in the maze)
@@ -16,14 +15,17 @@ def initGraph():
     graph = nx.grid_2d_graph(8,8)
     return graph
 
-def main():
-    graph = initGraph()
+def displayGraph(graph):
     print(graph.edges())
     pos = dict( (n, n) for n in graph.nodes() )
     labels = dict( ((i, j), i + (8-1-j) * 10 ) for i, j in graph.nodes() )
     nx.draw_networkx(graph, pos=pos, labels=labels)
     plt.axis('off')
     plt.show()
+
+def main():
+    graph = initGraph()
+    displayGraph(graph)    
 
 if __name__ == '__main__':
     main()
