ef964c14f90dcf731797662dc28072715034d632
Robin Donnay <robin.donnay@etu.univ-lyon1.fr>
2019-04-29 09:28:29 +0200

    ...

--- testingcode/robots/twins/robotTwins.py
+++ testingcode/robots/twins/robotTwins.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, OUTPUT_C, OUTPUT_B,SpeedRPM, SpeedPercent, MoveTank
-from ev3dev2.sensor import INPUT_1, INPUT_4
-from ev3dev2.sensor.lego import TouchSensor, ColorSensor, UltrasonicSensor
+from ev3dev2.sensor import INPUT_1, INPUT_3, INPUT_4
+from ev3dev2.sensor.lego import TouchSensor, ColorSensor, UltrasonicSensor, GyroSensor
 from ev3dev2.led import Leds
 import os
 import time
+import sys
 from threading import Timer, Thread, Event
 from time import sleep
 
@@ -16,6 +17,7 @@ class RobotTwin:
     _rightMotor = None
     _colorSensor = None
     _ultrasonicSensor = None
+    _gyroSensor = None
     _nbRotationsFor360turnWithOneMotor = 7.8           #à modifier avec les tests, probablement <
     _actualColor = 'q'
     _rightColor = 'q'
@@ -23,35 +25,35 @@ class RobotTwin:
     _stopDetectColor = False
 
     # METHODS
-    def __init__(self, leftMotor, rightMotor, colorSensor, ultrasonicSensor):
+    def __init__(self, leftMotor, rightMotor, colorSensor, ultrasonicSensor, gyroSensor):
         self._motors = MoveTank(leftMotor, rightMotor)
         self._leftMotor = LargeMotor(leftMotor)
         self._rightMotor = LargeMotor(rightMotor) 
         self._colorSensor = ColorSensor(colorSensor)
         self._ultrasonicSensor = UltrasonicSensor(ultrasonicSensor)
-        os.system('setfont ' + 'Lat15-Terminus24x12')
-        #self._leftMotor.stop_action = "brake"
-        #self._rightMotor.stop_action = "brake"
+        self._gyroSensor = GyroSensor(gyroSensor)
+        self._gyroSensor.mode = GyroSensor.MODE_GYRO_CAL
+        self._gyroSensor.mode = GyroSensor.MODE_GYRO_ANG
+        
 
     def moveForwardOneSquare(self):
-        self.bothMotorsRotation(50,50, 1)
+        self.bothMotorsRotation(50,50, 1.8)
 
     def turnLeft(self):
-        self.bothMotorsRotation(50, -50, 0.52)
-        # t1 = Thread(target=self.leftMotorPositiveRotation, args=[leftPuissance,0.52])
-        # t1.start()
-        # t2 = Thread(target=self.rightMotorNegativeRotation, args=[rightPuissance,0.52])
-        # t2.start()
+        #self.bothMotorsRotation(50, -40, -0.6)
+        self.bothMotorsRotation(50, -40, -90/146) # 146 = angle après une rotation de moteur
+
+
     
     def turnRight(self):
-        self.bothMotorsRotation(50, -50, -0.52)
+        self.bothMotorsRotation(50, -40, 90/146)
         # t1 = Thread(target=self.rightMotorPositiveRotation, args=[rightPuissance,0.52])
         # t1.start()
         # t2 = Thread(target=self.leftMotorNegativeRotation, args=[leftPuissance,0.52])
         # t2.start()
 
     def turn180(self):
-        self.bothMotorsRotation(50, -50, 1.08)
+        self.bothMotorsRotation(50, -40, 180/146)
         # t1 = Thread(target=self.leftMotorPositiveRotation, args=[leftPuissance,1.08])
         # t1.start()
         # t3 = Thread(target=self.rightMotorNegativeRotation, args=[rightPuissance,1.08])
@@ -85,23 +87,10 @@ class RobotTwin:
 
     def stopMotors(self):
         self._motors.off
-
-    def runUntil10cm(self):
-        self._motors.run_forever()
-        self._motors.on(10, 10)
-        while(self._ultrasonicSensor.distance_centimeters>10.0):
-            print()
-        self.stopMotors()
-
-    def turnLeftWhenObstacle(self):
-        self._motors.run_forever()
-        self._motors.on(10,10)
-        while(self._ultrasonicSensor.distance_centimeters>10):
-            print()
-        self.stopMotors()
-        self.rightMotorPositiveRotation(10,1)
-        self.stopMotors()
     
+    def getAngle(self):
+        return self._gyroSensor.angle 
+
     def detectColor(self):
         raise NotImplementedError
 
@@ -122,18 +111,22 @@ class RobotTwin:
         self._motors.on(-5,-5)
 
 def main():
-    twin = RobotTwin(OUTPUT_A, OUTPUT_D, INPUT_1, INPUT_4)
-    
-    # thread must be run at first to start checking the ultra sonique sensor distance
-    t2 = Thread(target=twin.ultrasonicDetect, args=[])
-    t2.start()
-    
-    # thread which runs a common method
-    t1 = Thread(target=twin.leftMotorPositiveRotation, args=[50,1.08])
-    t1.start()
+    twin = RobotTwin(OUTPUT_A, OUTPUT_D, INPUT_1, INPUT_4, INPUT_3)
 
-    t3 = Thread(target=twin.rightMotorNegativeRotation, args=[50,1.08])
-    t3.start()
+    twin.turnLeft()
+    twin.turnRight()
+    twin.turn180()
+    twin.turn180()
+    # thread must be run at first to start checking the ultra sonique sensor distance
+    # t2 = Thread(target=twin.ultrasonicDetect, args=[])
+    # t2.start()
+    
+    # # thread which runs a common method
+    # t1 = Thread(target=twin.leftMotorPositiveRotation, args=[50,1.08])
+    # t1.start()
+
+    # t3 = Thread(target=twin.rightMotorNegativeRotation, args=[50,1.08])
+    # t3.start()
 
 if __name__ == '__main__':
     main()
