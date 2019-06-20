8d4a8796e7e3d3ce5c1ccb2a17936819d5e3082a
Robin Donnay <robin.donnay@etu.univ-lyon1.fr>
2019-06-20 14:26:28 +0200

    Update RobotTwin

--- testingcode/robots/twins/robotTwins.py
+++ testingcode/robots/twins/robotTwins.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from ev3dev2 import get_current_platform
-
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, OUTPUT_C, OUTPUT_B,SpeedRPM, SpeedPercent, MoveTank
 from ev3dev2.sensor import Sensor, INPUT_1, INPUT_2, INPUT_3, INPUT_4
 from ev3dev2.sensor.lego import TouchSensor, ColorSensor, UltrasonicSensor, GyroSensor
@@ -9,11 +8,9 @@ from ev3dev2.led import Leds
 import os
 import time
 import sys
+import random
 from threading import Timer, Thread, Event
 from time import sleep
-
-#sys.path.append('../../../include/nxt-python-2.2.2/nxt/sensor')
-#from hitechnic import Compass
 
 
 class RobotTwin:
@@ -26,12 +23,14 @@ class RobotTwin:
     _compassSensor = None
     _baseAngle = 0
     _actualAngle = 0
+    _isWallAhead = True
     _threadCompass = None
-    #_gyroSensor = None
+    _threadSonic = None
     _actualColor = 'q'
     _rightColor = 'q'
     _leftColor = 'q'
     _stopDetectColor = False
+    _stopThread = False
 
     # METHODS
     def __init__(self, leftMotor, rightMotor, colorSensor, ultrasonicSensor, gyroSensor):
@@ -44,47 +43,58 @@ class RobotTwin:
         sleep(1)
         self._baseAngle = self._compassSensor.value()
         self._actualAngle = self._baseAngle
-        self._threadCompass = Thread(target=self.setActualAngle, args=[])
-        # self._gyroSensor = GyroSensor(gyroSensor)
-        # self._gyroSensor.mode = GyroSensor.MODE_GYRO_CAL
-        # self._gyroSensor.mode = GyroSensor.MODE_GYRO_ANG
+        #self._threadCompass = Thread(target=self.setActualAngle, args=[])
+        self._threadSonic = Thread(target=self.setIsWallAhead, args=[])
+        #self._threadCompass.start()
+        self._threadSonic.start()
+        sleep(1)
+
         
     def moveForwardOneSquare(self):
-        self.bothMotorsRotation(50,50, 1.8)
+        if(not(self._isWallAhead)):
+            self.bothMotorsRotation(50,50, 1.8)
+            self.orientationCorrection()
 
     def turnLeft(self):
         #self.bothMotorsRotation(50, -40, -0.6)
-        self.bothMotorsRotation(50, -40, -90/146) # 146 = angle après une rotation de moteur
+        angleObjectif = (self._baseAngle+90)%360
+        self.bothMotorsRotation(50, -40, -90/146) # 146 = facteur de rotation d'après une rotation de moteur
+        time.sleep(1)
+        self._actualAngle = self._compassSensor.value()
+        self._baseAngle = angleObjectif
+        self.orientationCorrection()
 
     def turnRight(self):
         angleObjectif = (self._baseAngle-90)%360
-        while(self._actualAngle<angleObjectif-10 or self._actualAngle>angleObjectif+10):
-            self.runForever(50,-40)
-            #print("base : ", self._baseAngle, " va jusqu'a : ", angleObjectif, " evolution : ", self.getAngle(), file=sys.stderr)
-            self._actualAngle = self._compassSensor.value()
+        self.bothMotorsRotation(50, -40, 90/146) # 146 = facteur de rotation d'après une rotation de moteur
+        time.sleep(1)
+        self._actualAngle = self._compassSensor.value()
         self._baseAngle = angleObjectif
-        self.stopMotors()
+        self.orientationCorrection()
 
     def orientationCorrection(self):
         sleep(0.2)
         angleDif = self._baseAngle - self._compassSensor.value()
         if(angleDif<-1 or angleDif>1):
-            angleDif = self._baseAngle - self._compassSensor.value()
-            #while(self._compassSensor.value()!=angleDif):
             while(self._compassSensor.value()<self._baseAngle-1 or self._compassSensor.value()>self._baseAngle+1):
-                self.bothMotorsRotation(5, -4, 0.01)
-                sleep(0.1)
-
-
-
-            
+                if(self._compassSensor.value()>self._baseAngle and self._compassSensor.value()<self._baseAngle+180%360):
+                    self.bothMotorsRotation(5, -4, 2/146)
+                elif(self._compassSensor.value()<self._baseAngle and self._compassSensor.value()>self._baseAngle-180%360) :
+                    self.bothMotorsRotation(5, -4, -2/146)
+                else :
+                    #print("Je sais pas quoi faire", file=sys.stderr)
+                    self.bothMotorsRotation(5, -4, -2/146)
+                #print("objectif = ", self._baseAngle, " actuel = ", self._compassSensor.value(), file=sys.stderr)
+                sleep(0.5)
+                
 
     def turn180(self):
-        self.bothMotorsRotation(50, -40, 180/146)
-        # t1 = Thread(target=self.leftMotorPositiveRotation, args=[leftPuissance,1.08])
-        # t1.start()
-        # t3 = Thread(target=self.rightMotorNegativeRotation, args=[rightPuissance,1.08])
-        # t3.start()
+        angleObjectif = (self._baseAngle-180)%360
+        self.bothMotorsRotation(50, -40, 180/146) # 146 = facteur de rotation d'après une rotation de moteur
+        time.sleep(1)
+        self._actualAngle = self._compassSensor.value()
+        self._baseAngle = angleObjectif
+        self.orientationCorrection()
 
     def bothMotorsRotation(self, leftPuissance, rightPuissance, rotation):
         self._motors.on_for_rotations(SpeedPercent(leftPuissance), SpeedPercent(rightPuissance), rotation)
@@ -116,8 +126,8 @@ class RobotTwin:
         self._motors.off
     
     def getAngle(self):
-        #return self._gyroSensor.angle 
-        return self._actualAngle
+        self._actualAngle = self._compassSensor.value()
+        return self._compassSensor.value()
 
     def detectColor(self):
         raise NotImplementedError
@@ -139,18 +149,38 @@ class RobotTwin:
         self._motors.on(-5,-5)
 
     def setActualAngle(self):
-        while(True):
+        while(not(self._stopThread)):
             self._actualAngle = self._compassSensor.value()
+
+    def setIsWallAhead(self):
+        while(not(self._stopThread)):
+            self._isWallAhead = self._ultrasonicSensor.distance_centimeters<10
+            #print(self._ultrasonicSensor.distance_centimeters, file=sys.stderr)
 
 def main():
     twin = RobotTwin(OUTPUT_A, OUTPUT_D, INPUT_1, INPUT_4, INPUT_3)
 
-    #print(x, file=sys.stderr)
-    #print(x.value(), file=sys.stderr)
+    i=0
+    x=-1
+    while(i<100):
+        x = random.randrange(5)
+        if(x==0 or x==1):
+            twin.moveForwardOneSquare()
+        elif (x==2):
+            twin.turnLeft()
+        elif (x==3):
+            twin.turnRight()
+        else:
+            twin.turn180()
+        i+=1
+        print(i, file=sys.stderr)
+    twin._stopThread = True
 
+    #twin.turnLeft()
+    #twin.moveForwardOneSquare()
     #twin.turnRight()
-    twin.moveForwardOneSquare()
-    twin.orientationCorrection()
+    #twin.turn180()
+    #twin.moveForwardOneSquare()
 
     #print(x.mode, file=sys.stderr)
     #print(x.value(), file=sys.stderr)
