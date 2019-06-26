55635dd07773e9f16991b9d4458dfdeaf9fb847a
Robin Donnay <robin.donnay@etu.univ-lyon1.fr>
2019-06-26 17:18:37 +0200

    temp Update of twin robot

--- testingcode/chronometre.py
+++ testingcode/chronometre.py
@@ -6,18 +6,26 @@ class Chrono:
     # DATA
     _startTime = 0
     _endTime = 0
+    _lapsTime = 0
 
     # METHODS
     def __init__(self):
         self._startTime = 0
         self._endTime = 0
+        self._lapsTime = 0
 
     def start(self):
         self._startTime = time.time()
         return self._startTime
 
     def getTime(self):
-        return time.time() - self._startTime
+        return self._lapsTime + (time.time() - self._startTime)
+
+    def pause(self):
+        self._lapsTime += time.time() - self._startTime 
+
+    def restart(self):
+        self._startTime = time.time()
 
     def stop(self):
         self._endTime = time.time()
@@ -27,6 +35,7 @@ class Chrono:
     def reset(self):
         self._startTime = 0
         self._endTime = 0
+        self._lapsTime = 0
     
     def getChrono(self):
         return self._endTime - self._startTime
